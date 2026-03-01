bbd56b9a7e441abf727cbe4e8f403cc178eb138a
mingchuncai <3355290877@qq.com>
2026-03-01 23:32:49 +0800

    主题色定制

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,9 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [
+        ElementPlusResolver({importStyle: 'sass'}),
+      ],
     }),
   ],
   resolve: {
@@ -26,4 +28,14 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  css:{
+    preprocessorOptions: {
+      scss: {
+        additionalData:
+        `
+        @use "@/styles/element/index.scss" as *;
+        `,
+      }
+    }
+  }
 })
