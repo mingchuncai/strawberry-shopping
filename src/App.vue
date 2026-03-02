53e33b0a69b8a1fda58c2732b57dc7759f74426f
mingchuncai <3355290877@qq.com>
2026-03-02 21:27:29 +0800

    'scss文件自动导入'

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,30 @@ import { RouterView } from 'vue-router';
 
 <template>
   <router-view />
+  <div class="test">
+    test scss
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.test{
+  color: $priceColor;
+}
+
+header{
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo{
+  display:block;
+  margin:0 auto 2rem;
+}
+
+nav{
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+</style>
