38d963e60b4aba7db83d2dc2ac6b648328217913
mingchuncai <3355290877@qq.com>
2026-03-04 23:22:00 +0800

    '一级分类路由配置'

--- src/views/layout/components/LayoutFixed.vue
+++ src/views/layout/components/LayoutFixed.vue
@@ -17,7 +17,7 @@ const CategoryStore = categoryStore()
       <!-- 导航区域 -->
       <ul class="app-header-nav">
         <li class="home" v-for="item in CategoryStore.categorylist" :key="item.id">
-          <RouterLink to="/">{{item.name}}</RouterLink>
+          <RouterLink :to="`/category/${item.id}`">{{item.name}}</RouterLink>
         </li>
       </ul>
       <div class="right">
