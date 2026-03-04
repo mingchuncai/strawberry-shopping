47d6a0622d4a15818282b6470667df456c3c9d44
mingchuncai <3355290877@qq.com>
2026-03-04 20:42:41 +0800

    '插件封装'

--- src/views/home/HomeIndex.vue
+++ src/views/home/HomeIndex.vue
@@ -4,6 +4,7 @@ import HomeBanner from './components/HomeBanner.vue';
 import HomeProduct from './components/HomeProduct.vue';
 import HomeHot from './components/HomeHot.vue';
 import HomeNew from './components/HomeNew.vue';
+import HomePanel from './components/HomePanel.vue';
 </script>
 
 <template>
@@ -14,4 +15,16 @@ import HomeNew from './components/HomeNew.vue';
   <HomeNew/>
   <HomeHot/>
   <HomeProduct/>
+
+  <!-- test-->
+  <HomePanel title="新鲜好物" subtitle="新鲜好物 好多商品">
+    <div>
+      这是新鲜好物内部
+    </div>
+  </HomePanel>
+  <HomePanel title="人气推荐" subtitle="人气推荐 好多商品">
+    <div>
+      这是人气推荐内部
+    </div>
+  </HomePanel>
 </template>
---
+++ src/views/home/components/HomePanel.vue
@@ -0,0 +1,56 @@
+<script setup>
+//props
+defineProps({
+  title:{
+    type: String,
+    default: '标题'
+  },
+  subtitle:{
+    type: String,
+    default: '副标题'
+  }
+})
+</script>
+
+
+<template>
+  <div class="home-panel">
+    <div class="container">
+      <div class="head">
+        <!-- 主标题和副标题 -->
+        <h3>
+          {{ title }}<small>{{ subtitle }}</small>
+        </h3>
+      </div>
+      <!-- 主体内容区域 -->
+       <slot/>
+    </div>
+  </div>
+</template>
+
+<style scoped lang='scss'>
+.home-panel {
+  background-color: #fff;
+
+  .head {
+    padding: 40px 0;
+    display: flex;
+    align-items: flex-end;
+
+    h3 {
+      flex: 1;
+      font-size: 32px;
+      font-weight: normal;
+      margin-left: 6px;
+      height: 35px;
+      line-height: 35px;
+
+      small {
+        font-size: 16px;
+        color: #999;
+        margin-left: 20px;
+      }
+    }
+  }
+}
+</style>
