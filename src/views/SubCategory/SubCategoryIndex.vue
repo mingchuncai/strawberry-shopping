07d1e987f74eb2cfb1b39c2af9ebb09d26abc92f
mingchuncai <3355290877@qq.com>
2026-03-05 23:34:39 +0800

    二级分类-列表无限加载实现

--- src/views/SubCategory/SubCategoryIndex.vue
+++ src/views/SubCategory/SubCategoryIndex.vue
@@ -29,6 +29,25 @@ const getSubCategory = async () => {
 onMounted(() => {
   getSubCategory()
 })
+
+//tab切换回调
+const tabChange=()=>{
+  console.log("i am changed",reqData.value.sortField)
+  getSubCategory()
+  reqData.value.page=1
+}
+
+//加载更多
+const disabled=ref(false)
+const load = async () => {
+  reqData.value.page++
+  const res = await getSubCategoryapi(reqData.value)
+  goodlist.value = [...goodlist.value, ...res.result.items]
+  //finish stop
+  if (res.result.items.length==0) {
+    disabled.value = true
+  }
+}
 </script>
 
 <template>
@@ -43,12 +62,12 @@ onMounted(() => {
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <div class="body" v-infinite-scroll="load"  :infinite-scroll-disabled="disabled"> >
         <!-- 商品列表-->
          <GoodsItem v-for="goods in goodlist" :goods="goods" :key="goods.id"/>
       </div>
