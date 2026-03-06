a9157f60a0ab09e441e035bb2a299da10ab8cc80
mingchuncai <3355290877@qq.com>
2026-03-06 21:52:32 +0800

    登录-自定义校验规则

--- src/views/login/LoginIndex.vue
+++ src/views/login/LoginIndex.vue
@@ -5,7 +5,8 @@ import { ref} from 'vue'
 //1.准备表单对象
 const form=ref({
   account:'',
-  password:''
+  password:'',
+  agree:true
 })
 
 //2.准备规则对象
@@ -17,6 +18,18 @@ const rules={
   password:[
     {required:true,message:'密码不能为空',trigger:'blur'},
     {min:6,max:14,message:'密码长度为6-14位',trigger:'blur'}
+  ],
+  agree:[
+    {
+      validator: (rule,value,callback)=>{
+        //自定义逻辑
+        if(value){
+          callback()
+        }else{
+          callback(new Error('请勾选协议'))
+        }
+    }
+  }
   ]
 }
 </script>
@@ -51,8 +64,8 @@ const rules={
               <el-form-item prop="password" label="密码">
                 <el-input v-model="form.password"/>
               </el-form-item>
-              <el-form-item label-width="22px">
-                <el-checkbox size="large" >
+              <el-form-item prop="agree" label-width="22px">
+                <el-checkbox size="large" v-model="form.agree">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
