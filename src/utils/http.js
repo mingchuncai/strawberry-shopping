065d9e6be7d987539a0dee0b94fc5e463556eeb6
mingchuncai <3355290877@qq.com>
2026-03-07 18:08:40 +0800

    登录-token拦截器中携带

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,6 +2,7 @@
 import axios from "axios";
 import {ElMessage} from 'element-plus'
 import 'element-plus/es/components/message/style/css'
+import {userStore} from '@/stores/user'
 const httpinstance=axios.create({
   baseURL:' http://pcapi-xiaotuxian-front-devtest.itheima.net',
   timeout:5000
@@ -11,6 +12,13 @@ const httpinstance=axios.create({
 //拦截器
 //axios请求拦截器
 httpinstance.interceptors.request.use(config=>{
+  //1.从pinia get token
+  const userstore=userStore()
+  //2.拼接token
+  const token=userstore.userInfo.token
+  if(token){
+    config.headers.Authorization=`Bearer ${token}`
+  }
   return config
 } ,e=>Promise.reject(e))
 //axios响应式拦截器
